f0f46d663725e935e12fb8228041247be7f0355d
Nikasa <nikasa.1889@gmail.com>
2017-06-04 13:24:37 +0000

    Add Parse.py, the script to auto parse Shopgun and upload to Matify

---
+++ Parse.py
@@ -0,0 +1,24 @@
+from ShopgunAPI import ShopgunAPI
+import json
+import io
+
+shopgunAPI = ShopgunAPI(verbose=True);
+
+selectedBrandNames = ['coop mega', 'coop extra', 'coop marked', \
+                      'kiwi', 'meny', 'rema', 'matkroken',\
+                      'coop prix','joker', 'bunnpris', 'extra', 'spar']
+#selectedBrandNames = ['coop mega']
+selectedCatalogList = shopgunAPI.getCatalogs(selectedBrandNames=selectedBrandNames)
+
+from MatifyAPI import MatifyAPI
+matifyAPI = MatifyAPI()
+#Get offers in selected catalogs
+for catalogStore, catalogId, catalogRunFrom, catalogBrand in selectedCatalogList:
+    offerDescList = shopgunAPI.getOfferDescriptionsOfCatalog(catalogId, catalogStore)
+    JsonFileName = shopgunAPI.generateCatalogFileName(catalogBrand, catalogId, catalogRunFrom)
+    data = json.dumps(offerDescList, ensure_ascii=False)     
+    #json_file.write(unicode(data))
+    matifyAPI.uploadCatalog(brandName=catalogBrand, catalogData=data, catalogFileName=JsonFileName)
+    print "Done."
+
+
